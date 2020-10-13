d9c552bfd6940abb7e9e225915c3a52095735072
LexSava <67606622+LexSava@users.noreply.github.com>
2020-10-13 12:02:13 +0300

    init: resolve  brackets-task
    Resolve  brackets-task.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let newArr = [];
+  if (str.length % 2){
+    return false;
+  }
+  for (let i = 0; i < str.length; i++) {
+     for (let j = 0; j < bracketsConfig.length; j++) {
+         if (str[i] === bracketsConfig[j][1] && newArr[newArr.length - 1] === bracketsConfig[j][0]){
+          newArr.pop();
+         }else if (str[i] === bracketsConfig[j][0]){
+              newArr.push(str[i]);
+             }
+         }
+   }
+ return newArr.length === 0;
 }
+
+
